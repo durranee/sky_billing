93d30fd0b810c27008e0b00f63acd20808711112
durranee <durranee@hotmail.com>
2018-06-10 20:27:27 +0100

    bill.get_data() second passing test for json return

--- spec/bill_spec.rb
+++ spec/bill_spec.rb
@@ -6,5 +6,13 @@ describe Bill do
     it 'should respond to get_data with 2 argument' do
       expect(Bill).to respond_to(:get_data).with(2).arguments
     end
+
+    it 'should return json object when called' do
+      URI = double(parse: 'test')
+      JSON = double(parse: 'response body in json')
+      response = double(body: 'response body')
+      http_class = double(get_response: response)
+      expect(Bill.get_data('test_url', http_class)).to eq('response body in json')
+    end
   end
 end
