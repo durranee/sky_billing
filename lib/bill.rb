9eca4ca176171afb0ace9d4ea235524a8aa7f900
durranee <durranee@hotmail.com>
2018-06-10 20:28:00 +0100

    bill.get_data() completed, tests passing

--- lib/bill.rb
+++ lib/bill.rb
@@ -5,6 +5,9 @@ require_relative './statement'
 class Bill
 
   def self.get_data(url = 'http://safe-plains-5453.herokuapp.com/bill.json', http_class = Net::HTTP)
+    uri = URI.parse(url)
+    response = http_class.get_response(uri)
+    JSON.parse(response.body)
   end
 
 end
