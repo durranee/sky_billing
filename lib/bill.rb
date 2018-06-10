5ee3fe6f480e2c8c3177035a2740cee7cb75dbbe
durranee <durranee@hotmail.com>
2018-06-10 20:26:40 +0100

    bill.get_data() first passing test

---
+++ lib/bill.rb
@@ -0,0 +1,10 @@
+require 'net/http'
+require 'json'
+require_relative './statement'
+
+class Bill
+
+  def self.get_data(url = 'http://safe-plains-5453.herokuapp.com/bill.json', http_class = Net::HTTP)
+  end
+
+end
